ae07a24e013a7c3203d4ea9ee9bccdcc617836a9
Rastislav Pánis <rastislav.panis@gjensidige.no>
2025-10-16 11:49:42 +0200

    more tools

--- src/main/kotlin/com/example/aiassistantdemo/service/ToolsDataService.kt
+++ src/main/kotlin/com/example/aiassistantdemo/service/ToolsDataService.kt
@@ -1,5 +1,7 @@
 package com.example.aiassistantdemo.service
 
+import com.example.aiassistantdemo.model.Claim
+import com.example.aiassistantdemo.model.Product
 import dev.langchain4j.agent.tool.Tool
 import org.springframework.stereotype.Service
 
@@ -9,5 +11,15 @@ class ToolsDataService(val dataService: DataService) {
     @Tool("Get user name")
     fun getUserName(): String {
         return dataService.getUserName()
+    }
+
+    @Tool("Get user products")
+    fun getUserProducts(): List<Product> {
+        return dataService.getProducts()
+    }
+
+    @Tool("Get user claims")
+    fun getUserClaims(): List<Claim> {
+        return dataService.getClaims()
     }
 }
